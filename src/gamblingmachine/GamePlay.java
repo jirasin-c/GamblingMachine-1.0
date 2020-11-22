fa9317cfee8adac673d67a4e4758850bb125a403
tnwnl <tanawat.naklor@mail.kmutt.ac.th>
2020-11-22 20:50:27 +0700

    Create menu.

--- src/gamblingmachine/GamePlay.java
+++ src/gamblingmachine/GamePlay.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package gamblingmachine;
-
+import java.util.Scanner;
 /**
  *
  * @author qbrickzx
@@ -15,7 +15,30 @@ public class GamePlay {
     }
 
     public static void menu() {
-        
+        Player James = new Player("James", 1, 0);
+        GamePlay game1 = new GamePlay();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Welcome to Gambling machine 1.0");
+        System.out.println("Please select game to start");
+        System.out.println("1. Card game");
+        System.out.println("2. Slot machine");
+        System.out.println("3. Exit program");
+
+        int selection = sc1.nextInt();
+        switch (selection) {
+            case 1:
+                System.out.println(playCard());
+                break;
+            case 2:
+                GamePlay.slot();
+                break;
+            case 3:
+                System.out.println("Program ended");
+                System.exit(0);
+            default:
+                System.out.println("--Invalid input-- : Try again");
+                GamePlay.menu();
+        }
     }
     public static String playCard() {
         return "";

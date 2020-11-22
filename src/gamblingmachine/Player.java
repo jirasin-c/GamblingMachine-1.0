74b3464c30a7595b9bec890736b69cfacb74ec76
tnwnl <tanawat.naklor@mail.kmutt.ac.th>
2020-11-22 16:35:42 +0700

    Create class player for play a game.

--- src/gamblingmachine/Player.java
+++ src/gamblingmachine/Player.java
@@ -10,5 +10,13 @@ package gamblingmachine;
  * @author qbrickzx
  */
 public class Player {
+    public static String name = "";
+    public static int id;
+    public static double money;
     
+    public Player(String name, int id, double money){
+        this.name = name;
+        this.id = id;
+        this.money = money;
+    }
 }

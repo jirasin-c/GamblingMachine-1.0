10395364568edc02b1ea6772575ba76a0b710525
Naruebet <Naruebet@James-T88LI7DI>
2020-11-22 18:26:41 +0700

    James test print

--- src/gamblingmachine/GamblingMachine.java
+++ src/gamblingmachine/GamblingMachine.java
@@ -17,6 +17,7 @@ public class GamblingMachine {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello KEW");
+        System.out.println("Hello James");
     }
     
 }

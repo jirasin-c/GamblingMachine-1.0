0912620d63ea8147a0e7a663f031f54877372236
tnwnl <tanawat.naklor@mail.kmutt.ac.th>
2020-11-22 16:46:46 +0700

    Change money to coins.

--- src/gamblingmachine/Player.java
+++ src/gamblingmachine/Player.java
@@ -12,11 +12,11 @@ package gamblingmachine;
 public class Player {
     public static String name = "";
     public static int id;
-    public static double money;
+    public static int coins;
     
     public Player(String name, int id, double money){
         this.name = name;
         this.id = id;
-        this.money = money;
+        this.coins = coins;
     }
 }

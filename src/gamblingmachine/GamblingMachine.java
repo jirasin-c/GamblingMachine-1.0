c9a5f58e7e6b7c5b8f8818d4ae5a4258eac7d432
Thanakrit <thanakrit.cankha@mail.kmutt.ac.th>
2020-11-23 16:41:24 +0700

    Complete version fixed main method.

--- src/gamblingmachine/GamblingMachine.java
+++ src/gamblingmachine/GamblingMachine.java
@@ -15,8 +15,7 @@ public class GamblingMachine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GamePlay game1 = new GamePlay();
-        game1.menu();
+        GamePlay.menu();
     }
     
 }
